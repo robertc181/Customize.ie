6d9282be50f1d3e284960c37a83e08db812e43a6
robertc181 <rdancole@gmail.com>
2021-01-27 16:41:15 +0000

    fixes to javascript for colour change

--- assets/js/secondaryColourChange.js
+++ assets/js/secondaryColourChange.js
@@ -1,22 +1,76 @@
-localStorage.setItem('colour', 'red');
 
-document.getElementById("bluebtn").onclick = function(){
-    localStorage.getItem("colour")
-debugger;
-    if(localStorage.getItem("colour") === "red"){
+// localStorage.setItem('colour', 'red');
+
+// let x = localStorage.getItem('colour');
+
+//  document.getElementById("bluebtn").onclick = function(){
+//      localStorage.getItem("colour")
+//  debugger;
+//      if(localStorage.getItem("colour") === "red"){
         
-         let findRed = document.getElementsByClassName("red");
+//           let findRed = document.getElementsByClassName("red");
  
-         for (let el of findRed) {
-            $(el).removeClass("red"); $(el).addClass("blue");
-             console.log(el)
-            
-                
-                
-        }
-    }
+//           for (let el of findRed) {
+//              $(el).removeClass("red"); $(el).addClass("blue");
+//              console.log(el)
+                    
+//          }
+//      }
 
-}
+//  }
+debugger;
+ localStorage.setItem('colour', 'red');
+
+ document.getElementById("bluebtn").onclick = function(){
+    let currentColour = "." + localStorage.getItem('colour');
+
+    console.log(currentColour);
+
+    let findClass = document.querySelectorAll(currentColour)
+
+    console.log(findClass);
+
+    for (let el of findClass) {
+        
+            let currentColour = localStorage.getItem('colour')
+            
+             $(el).removeClass(currentColour); $(el).addClass("blue");
+             console.log(el)
+         }
+
+
+
+
+
+    // let findClass = document.getElementsByClassName(currentColour);
+
+    // for (let el of findClass) {
+    //     $(el).removeClass(currentColour); $(el).addClass("blue");
+    //     console.log(el)
+    // }
+
+ }
+
+ console.log(currentColour)
+
+
+// document.getElementById("redbtn").onclick = function(){
+//     localStorage.getItem("colour")
+// debugger;
+//     if(localStorage.getItem("colour") === "red"){
+        
+//          let findRed = document.getElementsByClassName("red");
+ 
+//          for (let el of findRed) {
+//             $(el).removeClass("red"); $(el).addClass("blue");
+//              console.log(el)
+                    
+//         }
+//     }
+
+//     localStorage.setItem('colour', 'blue');
+
+// }
 // debugger;
 // // var elems = document.querySelectorAll("red");
 // var elems = document.querySelectorAll(".red");
